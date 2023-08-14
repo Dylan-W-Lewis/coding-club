850c948df7561b8fc0fca3a3e12fd395f0227bcb
Dylan-W-Lewis <2808003L@student.gla.ac.uk>
2023-08-14 11:52:33 +0100

    My solution for challenge 3
    Solution for 'Jump the Five' challenge in Python

---
+++ jump.py
@@ -0,0 +1,18 @@
+import argparse
+parser = argparse.ArgumentParser(prog="Jump the five",
+                                 description="Encode numbers in a string using 'jump the five' algorithm")
+parser.add_argument("string", action="store", help="Input text")
+
+args = parser.parse_args()
+
+dict = {"1": "9", "2": "8", "3": "7", "4": "6", "6": "4", "7": "3", "8": "2", "9": "1", "5": "0", "0": "5"}
+
+strs = list(args.string)
+replacement = []
+
+for s in strs:
+    replacement.append(dict.get(s,s))
+
+outpt = ""
+outpt = outpt.join(replacement)
+print(outpt)
